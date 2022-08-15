33f311353f63980e2d2c73d0ea94602723703f8e
MaheshBangar595 <maheshbangar595@gmail.com>
2022-08-15 15:04:15 +0530

    BNK-2: BankApplicationProject

---
+++ src/SignUp/Account.java
@@ -0,0 +1,87 @@
+package SignUp;
+
+public class Account {
+
+	private int pin;
+	private long accNo;
+	private String accName;
+	private String aadharcard;
+	private String pancard;
+	private String address;
+	private long mbNo;
+	private double accBalance;
+	private int monthlyIncome;
+
+	public int getpin() {
+		return pin;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+	public long getAccNo() {
+		return accNo;
+	}
+
+	public void setAccNo(long accNo) {
+		this.accNo = accNo;
+	}
+
+	public String getAccName() {
+		return accName;
+	}
+
+	public void setAccName(String accName) {
+		this.accName = accName;
+	}
+
+	public String getAadharCard() {
+		return aadharcard;
+	}
+
+	public void setAadharcard(String aadharcard) {
+		this.aadharcard = aadharcard;
+	}
+
+	public String getPanCard() {
+		return pancard;
+	}
+
+	public void setPancard(String pancard) {
+		this.pancard = pancard;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getMbNo() {
+		return mbNo;
+	}
+
+	public void setMbNo(long mbNo) {
+		this.mbNo = mbNo;
+	}
+
+	public double getAccBalance() {
+		return accBalance;
+	}
+
+	public void setAccBalance(double accBalance) {
+		this.accBalance = accBalance;
+	}
+
+	public int getMonthlyIncome() {
+		return monthlyIncome;
+	}
+
+	public void setMonthlyIncome(int monthlyIncome) {
+		this.monthlyIncome = monthlyIncome;
+	}
+
+}
